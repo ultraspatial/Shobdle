ccb678196e578f6bcd3a5c87c92295e473ae5452
ultraspatial <ahnafzaman2006@gmail.com>
2025-08-05 02:22:06 +0600

    v0.2.1 Cleaned up python script for release.

--- FilterWords.py
+++ FilterWords.py
@@ -4,10 +4,10 @@ import unicodedata
 _BASE_CONSONANT_RANGE = range(0x0995, 0x09B9 + 1)
 _BASE_CONSONANTS = {chr(code) for code in _BASE_CONSONANT_RANGE}
 _INDEPENDENT_VOWELS = {chr(code) for code in range(0x0985, 0x0994 + 1)}
-
+WordFile = 'BengaliWordList_48.txt' # PS Use BanglaWordList_48.txt from github/MinhasKamal when running for the  first time.
 
 def filter_4_char_words():
-    with open('BengaliWordList_48.txt') as f:
+    with open(WordFile) as f:
         words = f.read().split('\n')
         four_char_words = [word for word in words if len(word) == 4]
         return four_char_words
@@ -22,7 +22,7 @@ def count_main_letters(word):
 
 def filter_two_main_letter_words(word_list):
     return [word for word in word_list if count_main_letters(word) == 2]
-
+filtered_words
 
 if __name__ == '__main__':
     filtered_words = filter_two_main_letter_words(filter_4_char_words())
