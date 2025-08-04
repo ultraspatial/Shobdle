07a7b0156bcf8c35221c22a9c63e3cbd4bbc9a57
ultraspatial <ahnafzaman2006@gmail.com>
2025-08-05 00:18:15 +0600

    v0.1.1 implement 4 char filter

--- process.py
+++ process.py
@@ -1,5 +1,9 @@
-f = open("BengaliWordList_48.txt")
-words= f.read().split("\n")
-for word in words:
-    if len(word) == 4:
-        print(word)
+import unicodedata
+import regex
+
+def Filter4CharWords():
+    with open("BengaliWordList_48.txt") as f:
+        words= f.read().split("\n")
+        four_char_words = [word for word in words if len(word) == 4]
+
+def Filter2ConsonantWords():
